88581319a701582855f3a6d9d454aff5d668ee3d
pereramj <135605730+pereramj@users.noreply.github.com>
2024-01-31 20:01:32 +0530

    Add files via upload

---
+++ q5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+
+        for(int i=1; i<=12; i++)
+            {
+                Console.WriteLine(number + " x " + i + " = " + number* i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ q6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Name : ");
+            String name = Console.ReadLine();
+            String Grade = "";
+
+            Console.Write("Enter Marks : ");
+            int marks = int.Parse(Console.ReadLine());
+
+            if (marks > 100)
+            {
+                Console.WriteLine("Wrong MArks !");
+            }
+            else if (marks >= 75)
+            {
+                Grade = "A";
+            }
+            else if (marks >= 65)
+            {
+                Grade = "B";
+            }
+            else if (marks >= 55)
+            {
+                Grade = "C";
+            }
+            else if (marks >= 45)
+            {
+                Grade = "D";
+            }
+            else
+            {
+                Grade = "Failed";
+            }
+            Console.WriteLine(name + " Your Grade is " + Grade);
+
+            Console.ReadLine();
+        }
+           
+    }
+}
